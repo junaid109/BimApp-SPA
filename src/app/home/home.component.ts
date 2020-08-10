d635c82ad0625633d676d7fce577b9838f588f6e
Junaid Malik <junmalik109@gmail.com>
2020-08-10 20:54:06 +0100

    [Auth] Mock auth service component config

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,25 +8,18 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HomeComponent implements OnInit {
   registerMode = false;
-  values: any;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.getValues();
+
   }
 
   registerToggle() {
     this.registerMode = true;
   }
 
-  getValues() {
-    this.http.get("https://localhost:44313/api/values").subscribe(response => {
-      this.values = response;
-    }, error => {
-      console.log(error);
-    });
-  }
+
 
   cancelRegisterMode(registerMode: boolean)
   {
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -6,16 +7,20 @@ import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  @Input() valuesFromHome: any;
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 
   register() {
+    this.authService.register(this.model).subscribe(() => {
+      console.log('registration successful');
+    }, error => {
+      console.log(error);
+    });
     console.log('registered');
   }
 
