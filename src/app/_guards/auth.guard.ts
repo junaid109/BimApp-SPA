d615a32a669e930d13aaec062c64da794b8f3cdf
Junaid Malik <junmalik109@gmail.com>
2021-02-08 21:14:53 +0000

    [Auth] auth guard service added

---
+++ src/app/_guards/auth.guard.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../_services/auth.service';
+import { ToastrService } from 'ngx-toastr';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  /**
+   *
+   */
+  constructor(private authService: AuthService, private toastr: ToastrService) {
+
+
+  }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.authService.currentUser$.pipe(
+      map(user => {
+        if (user) return true;
+        this.toastr.error("Error");
+      })
+    );
+  }
+
+}
