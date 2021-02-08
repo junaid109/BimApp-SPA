12b0960e91e007842542bb8f5d01c0035f98f6ce
Junaid Malik <junmalik109@gmail.com>
2021-02-08 21:12:52 +0000

    [Components] model view and messages page comps

---
+++ src/app/modelviewer/modelviewer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modelviewer',
+  templateUrl: './modelviewer.component.html',
+  styleUrls: ['./modelviewer.component.css']
+})
+export class ModelviewerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
