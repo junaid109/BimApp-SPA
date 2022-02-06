a17d95880c3ad17434f50b44f6c6d1ced72a6df4
Junaid Malik <junmalik109@gmail.com>
2022-02-06 18:14:57 +0000

    member detail component styling and layout

--- src/app/members/member-detail/member-detail.component.ts
+++ src/app/members/member-detail/member-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Member } from '../../_models/member';
+import { MembersService } from '../../_services/members.service';
+import { SharedModule } from 'src/app/_modules/shared.module';
 
 @Component({
   selector: 'app-member-detail',
@@ -6,10 +10,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-detail.component.css']
 })
 export class MemberDetailComponent implements OnInit {
+  member: Member;
 
-  constructor() { }
+  constructor(private memberService: MembersService, private route: ActivatedRoute, private shared: SharedModule) { }
 
   ngOnInit(): void {
+    this.loadMember();
+  }
+
+  loadMember() {
+    this.memberService.getMember(this.route.snapshot.paramMap.get('username')).subscribe(member =>
+      {
+        member = member;
+      })
   }
 
 }
